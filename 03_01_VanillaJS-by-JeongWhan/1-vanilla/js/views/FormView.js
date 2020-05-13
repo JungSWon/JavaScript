5e53975d9abc30573101e094f302e2e5a36db8b7
JungSWon <flowerneco@gmail.com>
2020-05-14 08:55:35 +0900

    show X btn if keyup on search form

--- 03_01_VanillaJS-by-JeongWhan/1-vanilla/js/views/FormView.js
+++ 03_01_VanillaJS-by-JeongWhan/1-vanilla/js/views/FormView.js
@@ -9,18 +9,25 @@ const FormView = Object.create(View)
 FormView.setup = function(el){
   this.init(el) // /View.js에서 View.init(el) 함수를 만들어 놨다
   this.inputEl = el.querySelector('[type=text]') // el 찾아서 정의  
-  this.resetEl = el.querySelector('[type=reset]')
-
-  // resetEl을 숨김 처리하기 위한 함수 실행 
-  // 숨길거니까 인자는 false 
-  this.showResetBtn(false) 
+  this.resetEl = el.querySelector('[type=reset]')  
+  this.showResetBtn(false) // resetEl을 숨김 처리하기 위한 함수 실행: 숨길거니까 인자는 false
+  this.bindEvents()
+  return this 
   }
 
   // resetEl을 숨김 처리하기 위한 함수 생성
   // show라는 불리언값(디폴트 true)를 인자로 받는다. 
   FormView.showResetBtn = function(show = true){
     this.resetEl.style.display = show ? 'block' :'none'
-}
+  }
+
+  FormView.bindEvents = function(){
+    this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+  }
+  
+  FormView.onKeyup = function(){
+    this.showResetBtn(this.inputEl.value.length) // length가 있을 경우에는 true로 인자 넘겨질 예정 
+  }
 
 // FormView를 컨트롤러에서 쓰기 위해 익스포트 
   //MainController 에서 사용할 예정  
