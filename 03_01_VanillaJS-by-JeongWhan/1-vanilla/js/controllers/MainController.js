4c14e6c7b272cce320bc6d0df7bf07271879c54c
JungSWon <flowerneco@gmail.com>
2020-05-14 10:38:34 +0900

    send input val to MainController on keyup enter // [ISSUE] when enterkey is pressed, input val is reset,,MainController gets just empty val

--- 03_01_VanillaJS-by-JeongWhan/1-vanilla/js/controllers/MainController.js
+++ 03_01_VanillaJS-by-JeongWhan/1-vanilla/js/controllers/MainController.js
@@ -8,5 +8,14 @@ export default {
   init() {
     console.log(tag, 'init()')
     FormView.setup(document.querySelector('form'))
+      // .on('@submit', function(e){
+      //   console.log(e.detail.input,'?')
+      //   this.onSubmit(e.detail.input)})
+      .on('@submit', e => this.onSubmit(e.detail.input))
+      // Formview에 .on()메소드를 chaining했다. 
+      // 그게 가능하려면 FormView.setup에서 return this 해야한다
+  },
+  onSubmit(input) {
+    console.log(tag,'onSubmit()',input)
   }
 } 
--- 03_01_VanillaJS-by-JeongWhan/1-vanilla/js/views/FormView.js
+++ 03_01_VanillaJS-by-JeongWhan/1-vanilla/js/views/FormView.js
@@ -25,8 +25,15 @@ FormView.setup = function(el){
     this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
   }
   
-  FormView.onKeyup = function(){
+  FormView.onKeyup = function (e) {
+    const enter = 13
     this.showResetBtn(this.inputEl.value.length) // length가 있을 경우에는 true로 인자 넘겨질 예정 
+    if (e.keyCode !== enter) return
+    // FormView 는 검색결과를 보여주지 않아도 된다. 
+    this.emit('@submit', { input: this.inputEl.value })
+    // MainController에게 검색어를 주면 
+    // MainController는 검색어를 ResultView로 전달해줄 예정 
+    // 검색결과는 ResultView가 그리는 것
   }
 
 // FormView를 컨트롤러에서 쓰기 위해 익스포트 
