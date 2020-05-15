7b73d6358275a4da97dc215bced342964e8fb074
JungSWon <flowerneco@gmail.com>
2020-05-15 15:37:09 +0900

    0515 : delete todo

--- 03_Vanilla-clone-Chrome-by-Nomad/src/todo.js
+++ 03_Vanilla-clone-Chrome-by-Nomad/src/todo.js
@@ -1,21 +1,41 @@
 const todoForm = document.querySelector(".js-todoForm")
 const todoInput = todoForm.querySelector('.todo-input')
 const todoList = document.querySelector('.js-todoList')
-const savedList = JSON.parse(localStorage.getItem('todos'))
+let savedList = JSON.parse(localStorage.getItem('todos'))
 
-function saveTodo(val){
-  if (savedList === null ){
-    localStorage.setItem('todos',JSON.stringify(new Array))
-  }
-  savedList.push(val)
+function delTodo(id){
+  const newList = savedList.filter(element => element.id !== parseInt(id) )
+  savedList = newList
   localStorage.setItem('todos',JSON.stringify(savedList))
 }
 
-function paintTodo(val){
-  console.log(val)
+function handleDelete(event){
+  delLi = event.target.parentNode
+  console.log(delLi.id,delLi, '!!!')
+  todoList.removeChild(delLi)
+  delTodo(delLi.id)
+}
+
+function saveTodo(val,id){
+  const obj = {
+    "id": id, "todo" : val
+  }
+  if (savedList === null ){
+    localStorage.setItem('todos',JSON.stringify([obj]))
+    savedList = [obj]
+  }
+  else{
+    savedList.push(obj)
+    localStorage.setItem('todos',JSON.stringify(savedList))
+  }
+}
+
+function paintTodo(val,id){
   const addTodo = document.createElement('li')
   const delBtn = document.createElement('button')
   const span = document.createElement('span')
+  delBtn.addEventListener('click',handleDelete)
+  addTodo.setAttribute('id',id)
   delBtn.innerText = 'X'
   span.innerText = val
   addTodo.appendChild(span)
@@ -26,15 +46,16 @@ function paintTodo(val){
 function handleSubmit(e){
   e.preventDefault();
   const val = todoInput.value
-  paintTodo(val)
-  saveTodo(val)
+  const id = savedList !== null ? savedList[savedList.length-1].id+1 : 0
+  paintTodo(val,id)
+  saveTodo(val,id)
   todoInput.value = ''
 }
 
 function todoLoad(){
   if(savedList !== null){
     savedList.forEach(element => {
-      paintTodo(element)
+      paintTodo(element.todo, element.id)
     });
   }else{
   }
