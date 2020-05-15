c891ae529e82cf0f89a40801d7efb7c4666dba1a
JungSWon <flowerneco@gmail.com>
2020-05-15 09:15:51 +0900

    0515 : set greeting : localstorage, event listen , etc.

--- 03_Vanilla-clone-Chrome-by-Nomad/src/greeting.js
+++ 03_Vanilla-clone-Chrome-by-Nomad/src/greeting.js
@@ -4,16 +4,30 @@ const greeting = document.querySelector('.js-greetings')
 
 const User_LS = 'currentUser'
 
-function paintGreeting(test){
-    form.classList.remove('showing')
-    greeting.classList.add('showing')
-    greeting.innerText = `hello ${text}`
- }
+function handleSubmit(e){
+    e.preventDefault();
+    const currentVal = input.value
+    localStorage.setItem(User_LS,currentVal)
+    paintGreeting(currentVal)
+}
 
+function paintGreeting(text){
+    form.classList.remove('showing')
+    console.log(form)
+    greeting.classList.add('showing')
+
+    greeting.innerText = `hello ${text}`
+ } 
+
+function askForName(){
+    form.classList.add('showing')
+    form.addEventListener("submit",handleSubmit);
+}
 function loadName(){
     const currentUser = localStorage.getItem('currentUser');
-    if(currentUser === null ){
-
+    if(currentUser === null || currentUser === '' ){
+        console.log(currentUser)
+        askForName()
     }else {
         paintGreeting(currentUser);
     }
