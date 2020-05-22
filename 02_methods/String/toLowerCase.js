b6a55f3fd5346e83d85d932f3c4537fc205f4b91
JungSWon <flowerneco@gmail.com>
2020-05-22 17:15:46 +0900

    0522 : js method

---
+++ 02_methods/String/toLowerCase.js
@@ -0,0 +1,6 @@
+// 문자열을 소문자로 변환 
+
+str = 'QWERTY'
+res = str.toLowerCase()
+console.log("str: ",str) //QWERTY
+console.log("res: ",res) //qwerty
---
+++ 02_methods/String/toUpperCase.js
@@ -0,0 +1,6 @@
+// 문자열을 대문자로 변환 
+
+str = 'qwerty'
+res = str.toUpperCase()
+console.log("str: ",str) //qwerty
+console.log("res: ",res) //QWERTY
